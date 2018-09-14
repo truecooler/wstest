86c0cb854fd95bc5ac044dd63d38690c234588c6
TheCooler Lenovo <wiistar@yandex.ru>
2018-09-14 13:53:54 +0300

    изменение настроек

--- wstest/Globals.cs
+++ wstest/Globals.cs
@@ -10,7 +10,7 @@ namespace wstest
 {
     static class Globals
     {
-        static public string MysqlConnectionSettingsString = "server=192.168.10.7;user=cooler;database=ws;password=secret;";
+        static public string MysqlConnectionSettingsString = "server=vpn.thecooler.ru;user=cooler;database=ws;password=j8y3f4h5;";
         static public MySqlConnection MysqlConnection = null;
         static public MySqlCommand MysqlQuery = null;
         static public MySqlDataReader MysqlDataReader = null;
